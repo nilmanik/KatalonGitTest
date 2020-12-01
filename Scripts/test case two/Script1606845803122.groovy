7e348ab5f375d19ad88351bf6e365a1e678540d2
nilmanik <nilmani.kumar@tothenewcom>
2020-12-01 23:39:35 +0530

    test message

--- Scripts/test case two/Script1606845803122.groovy
+++ Scripts/test case two/Script1606845803122.groovy
@@ -15,9 +15,3 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('lifecharger.com')
-
-WebUI.closeBrowser()
-
---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1226b04b-f7d9-4e0c-8c07-574d2acd15d7</testSuiteGuid>
+   <testCaseLink>
+      <guid>ab52217c-c5d7-4a5e-89e6-c820c70dc9e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test case by member1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
