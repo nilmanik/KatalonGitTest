bf133e47abd7cf61bba6320e9fc18a81d9470ed3
nilmanik <nilmani.kumar@tothenewcom>
2020-12-01 23:58:26 +0530

    add new test case and test suite

---
+++ Test Suites/test suite2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ce2c9890-82f9-4b7b-8936-63c7eb7eee1d</testSuiteGuid>
+</TestSuiteEntity>
